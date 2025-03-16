44c0c3b5320ba67271e48d1ace65e10db3ae42a5
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-16 23:46:24 +0500

    Updated data

--- app/HomepageComponents/HeroData.tsx
+++ app/HomepageComponents/HeroData.tsx
@@ -18,37 +18,48 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { Jost } from 'next/font/google'
+import { CiSearch } from 'react-icons/ci';
+import { RiEBikeLine } from 'react-icons/ri';
 
 
 const HeroData = () => {
   return (
-    <div className=' flex flex-col justify-end space-y-3 w-full lg:w-[40%] pb-24'>
-    <div  className={`${jost.className} text-4xl    `}>Ride the Future, Own the Road</div> 
-    <div style={{fontWeight:400}}  className={`${jost.className} text-md  text-[#8c96a1]  `}>Explore the best motorbikes, accessories, and gear. Find your perfect ride today!</div> 
-     <div className='flex flex-col space-y-8 lg:w-[80%] bg-white p-4 rounded-md'>
-       {/*//! Input field */}
-       <Input  placeholder='Search by name'/>
-       {/*//! Select field */}
-         
-       <Select>
-     <SelectTrigger className="w-full">
-      <SelectValue placeholder="Search by brand" />
-     </SelectTrigger>
-      <SelectContent>
-        <SelectItem value="Yahama">Yahama</SelectItem>
-      <SelectItem value="Honda">Honda</SelectItem>
-      <SelectItem value="Suzuki">Suzuki</SelectItem>
-    </SelectContent>
-       </Select>
-         
-         <div style={{fontWeight:300}} className={`${jost.className} hover:cursor-pointer flex items-center justify-center bg-[#1967d2] hover:bg-blue-700 text-white duration-700 p-3 rounded-xl`}>Find Bike</div>
-         <div className='hidden   w-full   items-center justify-center space-x-7'>
-       <SiBmw size={70} />
-       <SiHonda size={70} />
-       <SiSuzuki size={65} />
-       </div>
-     </div>
-     </div>
+    // <div className='flex space-y-3 xl:space-y-6 flex-col absolute  top-[50%]  left-9 text-white  font-extrabold w-full'>
+    <div className='flex space-y-3 xl:space-y-6  flex-col lg:absolute lg:top-[50%] lg:left-9  lg:text-white  font-extrabold w-full bg-[#e5ebf5] lg:bg-transparent  text-[#3c3d42]  px-7 lg:px-0 lg:py-0 py-14 '>
+    <div style={{fontWeight:800}} className={`${jost.className} text-3xl lg:text-5xl xl:text-6xl 2xl:text-8xl`}>Ride the Future, Own the Road</div>
+    <div  style={{fontWeight:400}}  className='text-xl lg:text-2xl xl:text-4xl'>Explore the best motorbikes, accessories, and gear. Find your perfect ride today!</div>
+   
+   
+<div className=' flex flex-col lg:flex-row lg:w-[80%] 2xl:w-[50%] items-center justify-center bg-white  p-6 rounded-md space-y-6 lg:space-y-0 lg:space-x-3'>
+
+{/*//! Input field */}
+<div className='w-full flex items-center justify-center space-x-6'>
+<CiSearch size={25} />
+<input style={{fontWeight:400}}  className=' bg-white py-5 text-xl  text-black placeholder:text-slate-600 w-full focus:outline-none'  placeholder='Search by name'/>
+</div>    
+
+{/*//! Select field */}
+<div className='w-full flex items-center justify-center space-x-3'>
+<RiEBikeLine size={25} />
+
+<Select >
+<SelectTrigger style={{fontWeight:400}} className="w-full outline-none border-none shadow-none py-5 text-xl">
+<SelectValue placeholder="Search by brand" />
+</SelectTrigger>
+<SelectContent>
+<SelectItem value="Yahama">Yahama</SelectItem>
+<SelectItem value="Honda">Honda</SelectItem>
+<SelectItem value="Suzuki">Suzuki</SelectItem>
+</SelectContent>
+</Select>
+</div>
+{/*//! Button*/}
+
+<div style={{fontWeight:300}} className={`${jost.className} w-full hover:cursor-pointer flex items-center justify-center bg-[#1967d2] hover:bg-blue-700 text-white duration-700  text-xl p-6 rounded-md`}>Find Bike</div>
+
+    </div>
+
+</div>
   )
 }
 
--- app/HomepageComponents/Showlarge.tsx
+++ app/HomepageComponents/Showlarge.tsx
@@ -27,20 +27,21 @@ const Showlarge = () => {
     <div className='hidden  w-full h-full lg:block  flex-col relative '>     
     <img  className=' object-cover  w-full h-full max-h-[1500px] ' src="./bg-images.jpeg"/>
    </div>
-    <div className='flex space-y-3 xl:space-y-6 flex-col absolute  top-[50%]  left-9 text-white  font-extrabold w-full'>
+   <HeroData/>
+    {/* <div className='flex space-y-3 xl:space-y-6 flex-col absolute  top-[50%]  left-9 text-white  font-extrabold w-full'>
           <div style={{fontWeight:800}} className={`${jost.className} text-5xl xk:text-6xl 2xl:text-8xl`}>Ride the Future, Own the Road</div>
           <div  style={{fontWeight:200}}  className='text-xl xl:text-4xl'>Explore the best motorbikes, accessories, and gear. Find your perfect ride today!</div>
          
          
   <div className=' flex items-center justify-center bg-white w-5/6 p-3  rounded-md'>
 
-  {/*//! Input field */}
+
  <div className='w-full flex items-center justify-center spac-x-2'>
  <CiSearch />
  <input style={{fontWeight:400}}  className='bg-white py-5   text-black placeholder:text-slate-600 w-full focus:outline-none'  placeholder='Search by name'/>
  </div>    
 
- {/*//! Select field */}
+
   <Select >
 <SelectTrigger className="w-full outline-none border-none shadow-none py-5">
  <SelectValue placeholder="Search by brand" />
@@ -52,13 +53,13 @@ const Showlarge = () => {
 </SelectContent>
   </Select>
 
- {/*//! Button*/}
+
 
     <div style={{fontWeight:300}} className={`${jost.className} w-full hover:cursor-pointer flex items-center justify-center bg-[#1967d2] hover:bg-blue-700 text-white duration-700 p-6 rounded-md`}>Find Bike</div>
    
           </div>
   
-    </div>
+    </div> */}
    
 
  
