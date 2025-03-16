21205bc865ec5d1fc96390dd4c7abfbdc8560153
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-16 22:33:49 +0500

    Updated data

--- app/HomepageComponents/Hero.tsx
+++ app/HomepageComponents/Hero.tsx
@@ -13,15 +13,21 @@ const jost=Jost({
 
 const Hero = () => {
   return (
-    <div className=' w-full  flex items-center justify-center  px-11 pt-10'>
-       <div className='w-full max-w-screen-xl flex items-center justify-center '>
-     <HeroData/>
-         <div className='hidden  lg:block  flex-col  w-[70%] '>
-         
-        <img className=' h-[500px] w-full'  src="/BMW3.png" alt='Home-page-bike'/>
-
-      </div>
-       </div> 
+    <div className=' w-full flex items-center justify-center  relative'>
+      
+         <div className='hidden  w-full h-full lg:block  flex-col relative '>     
+          {/* <div className='w-full object-fill h-full  z-30 '> */}
+                 {/* f */}
+          {/* </div> */}
+         <img  className='object-cover  w-full h-full max-h-[80vh] ' src="./bg-images.jpeg"/>
+        </div>
+         <div className='flex flex-col absolute  top-1/3  left-9 text-white  font-extrabold'>
+               <div className={`${jost.className} text-6xl`}>Ride the Future, Own the Road</div>
+               <div>Explore the best motorbikes, accessories, and gear. Find your perfect ride today!</div>
+         </div>
+        
+         {/* <HeroData/> */}
+      
     </div>
   )
 }
