f8265ba678ac31c7e2e6794d3b657d43038db308
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-17 12:12:19 +0500

    Updated data

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
 const  Button=()=> {
-  return <InteractiveHoverButton className="rounded-sm w-full">Hover Me</InteractiveHoverButton>;
+  return <InteractiveHoverButton className="rounded-sm w-full bg-blue-500 text-white py-4">Find Bike</InteractiveHoverButton>;
 }
 
 export default Button;
--- components/logo-cloud.tsx
+++ components/logo-cloud.tsx
@@ -1,30 +1,9 @@
-import { Jost } from 'next/font/google'
-import React from 'react'
-import { SiDucati, SiHonda, SiKtm, SiSuzuki, SiYamahacorporation } from 'react-icons/si'
-import { CgBmw } from "react-icons/cg";
-import Button from '@/components/Button';
-const jost=Jost({
-    weight:['500'],
-    subsets:['latin']
-})
-import { InfiniteSlider } from '@/components/ui/infinite-slider'
-import { ProgressiveBlur } from '@/components/ui//progressive-blur'
-const PopularCompany = () => {
-  return (
-    // <div className='w-full flex items-center justify-center p-5 bg-[#f9fafb] '>
-    // <div className='w-full flex flex-col space-y-8 max-w-screen-lg  '>
-    // <div className={`${jost.className} text-[#32aa27]  text-3xl md:text-4xl`}>Popular Company</div>
-    // <div className='w-full grid grid-cols-3 lg:grid-cols-6 gap-x-4 gap-y-6 items-center justify-center'>
-    //     <div className='flex flex-col items-center hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300  '><SiHonda size={25} /> <span className={`${jost.className}`}>HONDA</span></div>
-    //     <div className='flex flex-col items-center hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300 '><SiYamahacorporation size={25}  /> <span className={`${jost.className}`}>YAMAHA</span></div>
-    //     <div className='flex flex-col items-center  hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300 '><SiSuzuki  size={25} /> <span className={`${jost.className}`}>SUZUKI</span></div>
-    //     <div className='flex flex-col items-center  hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300 '><CgBmw  size={25} /> <span className={`${jost.className}`}>BMW</span></div>
-    //     <div className='flex flex-col items-center  hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300 '><SiKtm  size={25} /> <span className={`${jost.className}`}>KTM</span></div>
-    //     <div className='flex flex-col items-center  hover:cursor-pointer justify-center hover:text-red-600 duration-500 border-b border-white hover:border-red-300 '><SiDucati  size={25} /> <span className={`${jost.className}`}>DUCATI</span></div>
-    // </div>
-    // </div>
-    // </div>
-    <section className="bg-background overflow-hidden py-16">
+import { InfiniteSlider } from '@/components/motion-primitives/infinite-slider'
+import { ProgressiveBlur } from '@/components/motion-primitives/progressive-blur'
+
+export default function LogoCloud() {
+    return (
+        <section className="bg-background overflow-hidden py-16">
             <div className="group relative m-auto max-w-7xl px-6">
                 <div className="flex flex-col items-center md:flex-row">
                     <div className="md:max-w-44 md:border-r md:pr-6">
@@ -127,17 +106,5 @@ const PopularCompany = () => {
                 </div>
             </div>
         </section>
-  )
+    )
 }
-
-export default PopularCompany
-
-
-// import { InfiniteSlider } from '@/components/motion-primitives/infinite-slider'
-// import { ProgressiveBlur } from '@/components/motion-primitives/progressive-blur'
-
-// export default function LogoCloud() {
-//     return (
-        
-//     )
-// }
