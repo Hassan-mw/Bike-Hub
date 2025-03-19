ceaf298125bd314ed5a6c0792ac75fe285c4d822
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-19 23:27:09 +0500

    Updated data

--- app/HomepageComponents/AppDownload.tsx
+++ app/HomepageComponents/AppDownload.tsx
@@ -12,19 +12,20 @@ const AppDownload = () => {
         <div className='w-full flex items-center justify-center  px-2'>
         <div className='w-full max-w-screen-xl flex items-center justify-center lg:space-x-16 lg:px-16 '>
             <div><img className='hidden lg:block  ' src="./mobile.png"/></div>
-        <div className='w-full  flex flex-col space-y-5 lg:space-y-10 items-center lg:items-start justify-center lg:justify-start p-5 '>
+       
+        <div className=' flex flex-col items-start justify-start space-y-5 lg:space-y-14 p-5'>
         <div className={`${jost.className} text-[#3579d7] text-md lg:text-xl`}>DOWNLOAD & ENJOY</div>    
-        <div className={`${jost.className} text-[#252629] text-2xl md:text-3xl lg:text-5xl font-semibold `}>Get the Bike-Hub  Bike <br/> Search App</div>    
+        <div className={`${jost.className} text-[#252629] text-2xl md:text-3xl lg:text-5xl font-semibold `}>Get the Bike-Hub  Bike  Search App</div>    
         <div className={`${jost.className} text-[#787878] text-sm lg:text-md`}>Search through millions of jobs and find the right fit. Simply swipe right to apply.</div>    
-        <div className='flex  items-center justify-center space-x-4'>
-            <div className='flex items-center justify-center space-x-4 bg-[#252629] hover:cursor-pointer duration-500 hover:bg-blue-500  rounded-md px-5 py-1'>
+        <div className='flex  items-center justify-center space-x-4 lg:space-x-8'>
+            <div className='flex items-center justify-center space-x-4 bg-[#252629] hover:cursor-pointer duration-500 hover:bg-blue-500  rounded-md px-5 py-1 lg:px-8 lg:py-3'>
                 <div className='text-white'><FaApple size={30} /></div>
                   <div className={`${jost.className} flex flex-col  text-white`}>
                     <div className='text-xs'>Download on the</div>
                     <div className='text-md'>Apple Store</div>  
                   </div>
             </div>
-            <div className='flex items-center justify-center space-x-4 bg-[#252629]  hover:cursor-pointer duration-500 hover:bg-blue-500   rounded-md px-5 py-1'>
+            <div className='flex items-center justify-center space-x-4 bg-[#252629]  hover:cursor-pointer duration-500 hover:bg-blue-500   rounded-md px-5 py-1  lg:px-8 lg:py-3'>
                 <div className='text-white'><FaGooglePlay size={28} /></div>
                   <div className={`${jost.className} flex flex-col  text-white`}>
                     <div className='text-xs'>Get in on</div>
@@ -33,10 +34,12 @@ const AppDownload = () => {
             </div>
 
         </div>
+        
+        </div>
         </div>
         </div>
     </div>
-    </div>
+  
   )
 }
 
