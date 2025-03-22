934818c99b66ae8b8a513e3db7291a8ed0ee9c45
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-22 22:01:26 +0500

    updating data

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
 const  Button=()=> {
-  return <InteractiveHoverButton className="rounded-sm w-full bg-blue-500 text-white py-4">Find Bike</InteractiveHoverButton>;
+  return <InteractiveHoverButton className="rounded-sm w-full bg-blue-500 text-white py-4 xl:py-5 2xl:rounded-xl 2xl:text-xl">Find Bike</InteractiveHoverButton>;
 }
 
 export default Button;
