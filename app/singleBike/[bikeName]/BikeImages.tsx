074c3289d147ebe090e57c9686e02383f29e17b2
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-23 07:43:53 +0500

    Updated data

---
+++ app/singleBike/[bikeName]/BikeImages.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/carousel"
+
+const BikeImages = () => {
+  return (
+    <Carousel className="w-full max-w-xl mb-36 overflow-hidden">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex items-center justify-center p-6 w-[500px] h-[400px]">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
+
+export default BikeImages
--- app/singleBike/[bikeName]/page.tsx
+++ app/singleBike/[bikeName]/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import BikeImages from './BikeImages'
 
 const page = () => {
   return (
     <div className='w-full flex items-center justify-center'>
-      <div className='w-full flex items-center justify-center max-w-screen-xl'>
-     hello gauys
-
+      <div className='w-full flex flex-col items-center justify-center max-w-screen-xl'>
+      <div ><BikeImages/> </div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
+     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsa quia nulla! Soluta nobis deserunt consequatur minus earum necessitatibus perferendis blanditiis nam ad, tempore ratione fuga fugit quae nisi nesciunt.</div>
       
       </div>
       </div>
