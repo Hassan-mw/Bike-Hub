7dbb7d52642a9a64b2c74bfefb06337c5142e840
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-24 22:14:24 +0500

    Updated data

--- app/API/Model/itemsModel.js
+++ app/API/Model/itemsModel.js
@@ -1,9 +1,28 @@
 const mongoose=require('mongoose');
+const { type } = require('os');
 
 
 const itemsModel=new mongoose.Schema({
 
+name:{
+    type:String,
+    require:[true,'A item must have name']
+},
+price:{
 
+    type:Number,
+    require:[true,'A item must have price']
+},
+company:{
+    type:String,
+    require:[true,'A item must belong to some company']
+},
+size:{
+    type:string,
+    enum:['small','medium','large']
+},
+color:string,
+matchedBike:string
 
 
 
