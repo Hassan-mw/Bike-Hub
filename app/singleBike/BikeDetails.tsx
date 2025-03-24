8bc168e71e48400a4a2d994b5f36137e82ccf730
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-24 08:41:12 +0500

    Updated data

--- app/singleBike/BikeDetails.tsx
+++ app/singleBike/BikeDetails.tsx
@@ -1,12 +1,14 @@
 import { Jost } from 'next/font/google';
 import React from 'react'
+import { BsFuelPumpFill } from 'react-icons/bs';
 import { GoDotFill } from "react-icons/go";
     
 const jost=Jost({
     weight:['500'],
     subsets:['latin-ext']
   })
-  
+
+import { GrPerformance } from "react-icons/gr";
 
 interface bikeDataType{
   brand:string;
@@ -20,7 +22,8 @@ interface bikeDataType{
   weight:number;
 }
 
-
+import { PiEngineBold } from "react-icons/pi";
+import { SiTransmission } from 'react-icons/si';
 
 const BikeDetails = ({brand,price,category,fuelType,mileage,model,name,topSpeed,weight}:bikeDataType) => 
   
@@ -37,9 +40,60 @@ const BikeDetails = ({brand,price,category,fuelType,mileage,model,name,topSpeed,
 ]
   return (
     <div className='w-full flex flex-col space-y-16 pb-24 p-8'>
-        <div className={`${jost.className} text-8xl w-full flex items-center justify-center  `}>
+        <div className={`${jost.className} text-6xl w-full flex items-center justify-center  `}>
           {name}
           </div>
+          {/* //!  Below Image detaisl */}
+
+        <div className='w-full grid grid-cols-2 md:grid-cols-3 place-content-center gap-7'>
+          {/* 1 */}
+         <div className='border-2 rounded-md w-full h-full flex flex-col space-y-5 items-center justify-between py-12 px-5'>
+          <div className='flex items-center justify-between w-full text-xl md:text-2xl'>
+          <div className={`${jost.className} `}>Enginer </div>
+        <div><PiEngineBold size={25}/></div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}>RPM</div>
+          <div className={`${jost.className} `}>5,500 </div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}>HP </div>
+          <div className={`${jost.className} `}>125 </div>
+          </div>
+        </div>
+          {/* 2 */}
+         <div className='border-2 rounded-md w-full h-full flex flex-col space-y-5 items-center justify-between py-12 px-5'>
+          <div className='flex items-center justify-between w-full text-xl md:text-2xl'>
+          <div className={`${jost.className} `}>Performance  </div>
+        <div><GrPerformance size={25}/></div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}> Torque</div>
+          <div className={`${jost.className} `}>170 Nm </div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}>Top Speed </div>
+          <div className={`${jost.className} `}>180 km/h</div>
+          </div>
+        </div>
+          {/* 3 */}
+         <div className='border-2 rounded-md w-full h-full flex flex-col space-y-5 items-center justify-between py-12 px-5'>
+          <div className='flex items-center justify-between w-full text-xl md:text-2xl'>
+          <div className={`${jost.className} `}> Fuel   </div>
+        <div><BsFuelPumpFill size={25}/></div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}> Fuel Type </div>
+          <div className={`${jost.className} `}>Petrol</div>
+          </div>
+          <div className='flex items-center justify-between w-full'>
+          <div className={`${jost.className} `}> Mileage</div>
+          <div className={`${jost.className} `}>15-20 km/l</div>
+          </div>
+        </div>
+        </div>
+
+
         <div className='grid grid-cols-1 lg:grid-cols-[70%_30%] gap-y-20 lg:gap-x-10'>
          <div className='lg:w-full   flex flex-col space-y-16 items-start justify-start text-xl'>
           <div className={` flex flex-col items-start justify-start space-y-8`}>
@@ -48,23 +102,26 @@ const BikeDetails = ({brand,price,category,fuelType,mileage,model,name,topSpeed,
           </div>
           <div className={`${jost.className} flex flex-col space-y-8 items-start justify-start`}>
            <div className={`${jost.className} text-3xl md:text-4xl`}>Key Features:</div>
-          <div className=' flex flex-col space-y-6 md:space-y-14 items-start justify-start text-md'>
-          <div style={{fontWeight:400}}   className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 text-[18px] md:text-xl `} ><span > <GoDotFill size={20} /> </span><span >1,099cc V4 Engine – Produces over 200 HP for thrilling performance.</span></div>  
-          <div style={{fontWeight:400}}   className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill  size={20} />  <span>Advanced Electronics – Includes traction control, wheelie control.</span></div>  
-          <div   style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Aero Winglets – Improves stability at high speeds.</span></div>  
-          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Lightweight Aluminum Frame – Ensures agile handling and cornering precision.</span></div>  
-          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Full TFT Display – Offers intuitive controls and smartphone connectivity.</span></div>  
-          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Advanced TFT Dash – Full-color display with smartphone connectivity.</span></div>  
-          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Multiple Riding Modes – Adapt to street, sport, and race conditions.</span></div>  
-          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 text-[18px]  md:text-xl`}><GoDotFill size={20} />  <span>Up & Down Quickshifter – Enables seamless clutchless gear shifts.</span></div>  
+          <div className=' flex flex-col space-y-6 md:space-y-8 items-start justify-start text-md'>
+          <div style={{fontWeight:400}}   className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 md:space-x-5 text-[18px] md:text-md `} ><span > <GoDotFill size={16} /> </span><span >1,099cc V4 Engine – Produces over 200 HP for thrilling performance.</span></div>  
+          <div style={{fontWeight:400}}   className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 md:space-x-5 text-[18px]  md:text-md`}><GoDotFill  size={16} />  <span>Advanced Electronics – Includes traction control, wheelie control.</span></div>  
+          <div   style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2 md:space-x-5 text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Aero Winglets – Improves stability at high speeds.</span></div>  
+          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 md:space-x-5 text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Lightweight Aluminum Frame – Ensures agile handling and cornering precision.</span></div>  
+          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2  md:space-x-5 text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Full TFT Display – Offers intuitive controls and smartphone connectivity.</span></div>  
+          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 md:space-x-5 text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Advanced TFT Dash – Full-color display with smartphone connectivity.</span></div>  
+          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start  md:items-center justify-center space-x-2 md:space-x-5 text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Multiple Riding Modes – Adapt to street, sport, and race conditions.</span></div>  
+          <div  style={{fontWeight:400}}  className={`${jost.className} text-[#80898f] flex items-start md:items-center justify-center space-x-2  md:space-x-5text-[18px]  md:text-md`}><GoDotFill size={16} />  <span>Up & Down Quickshifter – Enables seamless clutchless gear shifts.</span></div>  
             
           </div> 
 
          </div>
          </div>
        
+        {/* //!  Right */}
+        <div className='flex flex-col items-center justify-center space-y-10'>
+          {/* Top */}
         <div className='w-full  flex flex-col space-y-4 md:space-y-8 rounded-lg bg-[#f1f4fb] p-5  text-xl '>
-          <div style={{fontWeight:500}} className={`${jost.className} text-3xl`}>Bike Overview</div>
+          <div style={{fontWeight:500}} className={`${jost.className} w-full flex items-center justify-center text-3xl border-b-4 pb-3`}>Bike Overview</div>
           {  
           bikeDataArray.map((data,index)=>
          
@@ -74,6 +131,22 @@ const BikeDetails = ({brand,price,category,fuelType,mileage,model,name,topSpeed,
             </div>
               )  }
         </div>
+          {/* Bottom */}
+
+        <div className='w-full  flex flex-col space-y-4 md:space-y-8 rounded-lg bg-[#f1f4fb] p-5  text-xl '>
+          <div style={{fontWeight:500}} className={`${jost.className} w-full flex items-center justify-center text-3xl border-b-4 pb-3`}>Bike Overview</div>
+          {  
+          bikeDataArray.map((data,index)=>
+         
+            <div key={index} className={`${jost.className} flex items-center justify-between `}>
+               <div style={{fontWeight:400}} className={`${jost.className} `}>{data.property}</div> 
+               <div style={{fontWeight:400}} className={`${jost.className} text-[#80898f] `}>{data.value} {data?.postfix}</div> 
+            </div>
+              )  }
+        </div>
+        
+        
+        </div>   
  </div>
 
     </div>
--- app/singleBike/[bikeName]/BikeImages.tsx
+++ app/singleBike/[bikeName]/BikeImages.tsx
@@ -18,7 +18,8 @@ const BikeImages = ({image}:bikeDataType) => {
   return (
     <div className="w-full flex flex-col space-y-10">
         <div className="w-full flex items-center justify-center   overflow-hidden ">
-         <img className="w-full  max-w-[600px] scale-90 max-h-[400px]" src={`/${image}`} />
+         <img className="w-full  max-w-[800px] scale-90 max-h-[600px]" src={`/${image}`} />
+         {/* <div className="absolute top-0  hidden lg:block bg-green-500 rounded-full px-8 py-5"> v</div> */}
         </div>
      </div>
   )
