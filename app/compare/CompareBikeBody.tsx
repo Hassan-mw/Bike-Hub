80ab7ad0d5a6a815fc940e46d03304563ea41959
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-28 18:27:09 +0500

    Updated data

---
+++ app/compare/CompareBikeBody.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompareBikeBody = () => {
+  return (
+    <div>CompareBikeBody</div>
+  )
+}
+
+export default CompareBikeBody
--- app/compare/page.tsx
+++ app/compare/page.tsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { Jost } from 'next/font/google'
+import Link from 'next/link'
+import React, { Suspense } from 'react'
+import SideBarFilter from '../SideBarFilter/SideBarFilter'
+import SideBarData from '../SideBarFilter/SideBarData'
+import getAllBike from '../DataFetching/api'
+import Pagination from '@/components/Pagination'
+import CompareBikeBody from './CompareBikeBody'
 
-const page = () => {
+// import {getAllBike} from "@/app/DataFetching/api"
+
+interface BikeDataType {
+  _id:number;
+  name: string;
+  brand: string;
+  category: string;
+  price: number; // ✅ Fix: Change 'Number' to 'number'
+  mileage: number; // ✅ Fix: Change 'Number' to 'number'
+  topSpeed: number; // ✅ Fix: Change 'Number' to 'number'
+  weight: number; // ✅ Fix: Change 'Number' to 'number'
+  url: string;
+  fuelType: string;
+  slug: string;
+  status: string;
+  image:string;
+}
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
+const Loading=()=>{
+
+return (
+  <div className='w-full h-full flex items-center justify-center'>
+
+    
+    <div className="flex w-52 flex-col gap-4">
+    <div className="skeleton h-32 w-full"></div>
+    <div className="skeleton h-4 w-28"></div>
+    <div className="skeleton h-4 w-full"></div>
+    <div className="skeleton h-4 w-full"></div>
+  </div>
+  </div>
+
+)
+
+
+
+}
+
+
+
+const page = async({searchParams}:{searchParams:URLSearchParams}) => {
+
+  const filter=await searchParams
+ const result:{status:String,message:string,data:BikeDataType[]}=await getAllBike({filter})
+ console.log(result)
+ if(result.status==='fail'){
   return (
-    <div>page</div>
+    <div className='w-full flex items-center justify-center h-[90vh] text-4xl'>
+     There was some probleam in data fetching
+    </div>
+  )
+ }
+
+
+  return (
+    <div className='w-full '>
+            {/* Upper Body */}
+      <div className='w-full flex flex-col space-y-2 py-10 lg:py-20 items-center justify-center bg-[#f4f7fc] border-t'>
+       <div className={`${jost.className} text-5xl text-[#202124]`}>Bike List</div>
+       <div style={{fontWeight:400}} className={`${jost.className} text-2xl text-[#202124]`}> <Link className='text-[#888888]' href="/">Home</Link> / Bike List</div>
+      </div>
+      {/* Down Body */}
+      <div className='w-full flex flex-col items-center justify-center p-3  bg-white'>
+     <Suspense  fallback={<div className='w-full h-full flex items-center justify-center'>
+
+    
+<div className="flex w-52 flex-col gap-4">
+<div className="skeleton h-32 w-full"></div>
+<div className="skeleton h-4 w-28"></div>
+<div className="skeleton h-4 w-full"></div>
+<div className="skeleton h-4 w-full"></div>
+</div>
+</div>}>
+
+    
+      <CompareBikeBody/>
+     </Suspense>
+    </div>
+
+    </div>
+
   )
 }
+
 
 export default page
