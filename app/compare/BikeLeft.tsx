48118dcfdd1ceef01339ac9ed4be29a85714d01a
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-03-28 23:11:49 +0500

    Updated data

--- app/compare/BikeLeft.tsx
+++ app/compare/BikeLeft.tsx
@@ -1,25 +1,13 @@
-import { Jost } from 'next/font/google'
-import React from 'react'
-import { CiCirclePlus } from 'react-icons/ci'
+import BikeLeftSelector from "./BikeLeftSelector"
 
-
-const jost=Jost({
-  weight:['500'],
-  subsets:['latin']
-})
-
-
-const BikeLeft = () => {
+const BikeLeft =async () => {
+    const value="left";
   return (
     <div className='w-full flex flex-col space-y-3'>
-        <div className={`${jost.className} w-full flex items-center justify-center h-80 border-y-4 border-l-4 rounded-md rounded-r-none relative group hover:cursor-pointer `}>
-      <div className='w-full h-full flex flex-col space-y-2 items-center justify-center  '>
-        <div className='size-16 flex items-center justify-center border-4 text-2xl rounded-full group-hover:border-green-500  duration-500  '>+</div>
-        <div className={`${jost.className} text-sm group-hover:text-green-500 `}>Add Bike</div>
-      </div>
-       <div className={`${jost.className} absolute top-1/2 -right-7 text-2xl  size-12 border-4 bg-white rounded-full text-red-400 flex items-center justify-center`}>vs</div>
-        </div>
 
+    <div>
+      <BikeLeftSelector value={value}/>
+      </div> 
     </div>
   )
 }
---
+++ app/compare/BikeLeftSelector.tsx
@@ -0,0 +1,79 @@
+'use client';
+import { Jost } from 'next/font/google'
+import React, { useEffect, useState } from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { usePathname, useSearchParams } from 'next/navigation';
+import path from 'path';
+import { useRouter } from 'next/navigation';
+ 
+
+
+
+const jost=Jost({
+  weight:['500'],
+  subsets:['latin']
+})
+
+
+const BikeLeftSelector = ({value}:{value:string}) => {
+    const [selectBike,setSelectBike]=useState('none')
+    console.log(selectBike)
+    const pathname=usePathname();
+    const searchParams=useSearchParams()
+    const router=useRouter()
+ 
+
+    useEffect(()=>{
+        const params=new URLSearchParams(searchParams);
+        params.set(value,selectBike)
+          router.replace(`${pathname}?${params.toString()}`)
+    },[selectBike])
+  return (
+
+    <div className={`${jost.className} w-full flex items-center justify-center h-80 border-y-4 border-l-4 rounded-md rounded-r-none relative group hover:cursor-pointer `}>
+
+    <DropdownMenu >
+    <DropdownMenuTrigger asChild>
+      <div  className='border-none hover:cursor-pointer bg-none hover:bg-none shadow-none'> 
+      <div className='size-16 flex items-center justify-center border-4 text-2xl rounded-full group-hover:border-green-500  duration-500  '>+</div>
+    </div>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-56">
+      <DropdownMenuGroup>
+        
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+          <DropdownMenuPortal>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={()=>setSelectBike('YBR 125')}>Email</DropdownMenuItem>
+              <DropdownMenuItem>Message</DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuPortal>
+        </DropdownMenuSub>
+     
+
+      </DropdownMenuGroup>
+    </DropdownMenuContent>
+  </DropdownMenu>   
+    
+     <div className={`${jost.className} absolute top-1/2 -right-7 text-2xl  size-12 border-4 bg-white rounded-full text-red-400 flex items-center justify-center`}>vs</div>
+      </div>
+
+  )
+}
+
+export default BikeLeftSelector
