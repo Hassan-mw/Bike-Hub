c5028fc647d9c628a1a272f5367a29a5f3e9b94d
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-01 23:58:44 +0500

    Updated data

--- app/HomepageComponents/FeaturesBike.tsx
+++ app/HomepageComponents/FeaturesBike.tsx
@@ -14,41 +14,47 @@ const FeaturesBike = () => {
 
         {/* Upper Test */}
     <div className={`${jost.className} flex flex-col items-center justify-center space-y-3 `}>
-    <div className={`${jost.className} text-[#252629] text-3xl md:text-4xl border-b-4 pb-2 `}>Featured Bikes</div>
-   <div style={{fontWeight:400}} className={`${jost.className}  text-md text-[#a1a9b2] `}>Find the perfect ride that matches your lifestyle and adventure. </div> 
+    <div className={`${jost.className} text-[#252629] text-3xl md:text-4xl  `}>Featured Bikes</div>
+   <div style={{fontWeight:400}} className={`${jost.className}  text-md text-[#879099] `}>Find the perfect ride that matches your lifestyle and adventure. </div> 
     </div>
 
 
 
     {/* Data */}
     <div className='w-full grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 px-4'>
+    
       {/* 1 */}
-    <div   className='flex flex-col items-center justify-center p-4 bg-white rounded-md overflow-hidden border group relative border-green-500'>
-        <div style={{fontWeight:400}} className={`${jost.className} text-xl border-b`}> Best for Off-Road Adventures</div>
-         <img className='h-60  ' src="./user-1742579208356.jpeg" />
-         <div className='w-full flex flex-col items-center justify-center space-y-5 '>
-         <div className={`${jost.className} text-4xl tracking-widest `}>Honda 150</div>
-       
-    </div>
-    </div>
+      <div className="flex flex-col border p-3 rounded-md">
+      <div className={`${jost.className} w-full flex items-center justify-center text-xl`}> Electric-Powered for Effortless Rides</div>
+      <img className="h-60 sm:h-40  lg:h-72 w-full object-cover rounded-t-md " src="./c-1.jpeg"/>
+      <div className="flex flex-col items-center justify-center space-y-2 bg-white rounded-b-md border-t  p-3">
+      <div className={`${jost.className} text-md`}>Harley-Davidson Sportster 883</div>
+      <div style={{fontWeight:300}} className={`${jost.className} text-[#504d4d]`}>A classic cruiser motorcycle with a robust V-twin engine, known for its raw power and iconic design.</div>
+      </div>
+      </div>
+     
       {/* 2 */}
-    <div   className='flex flex-col items-center justify-center p-4 bg-white rounded-md overflow-hidden border group relative border-green-500'>
-        <div style={{fontWeight:400}} className={`${jost.className} text-xl border-b`}>Perfect for City Commuting</div>
-         <img className='h-60  ' src="./user-1742578949550.jpeg" />
-         <div className='w-full flex flex-col items-center justify-center space-y-5 '>
-         <div className={`${jost.className} text-4xl tracking-widest `}>Honda 150</div>
-       
-    </div>
-    </div>
-      {/* 3 */}
-    <div   className='flex flex-col items-center justify-center p-4 bg-white rounded-md overflow-hidden border group relative border-green-500'>
-        <div style={{fontWeight:400}} className={`${jost.className} text-xl border-b`}> Electric-Powered for Effortless Rides</div>
-         <img className='h-60  ' src="./user-1742580402393.jpeg" />
-         <div className='w-full flex flex-col items-center justify-center space-y-5 '>
-         <div className={`${jost.className} text-4xl tracking-widest `}>Honda 150</div>
-       
-    </div>
-    </div>
+      <div className="flex flex-col border p-3 rounded-md">
+      <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Perfect for City Commuting</div>
+      <img className="h-60 sm:h-40  lg:h-72 w-full object-cover rounded-t-md " src="./c-2.jpeg"/>
+      <div className="flex flex-col items-center justify-center space-y-2 bg-white rounded-b-md border-t  p-3">
+      <div className={`${jost.className} text-md`}>Harley-Davidson Sportster 883</div>
+      <div style={{fontWeight:300}} className={`${jost.className} text-[#504d4d]`}>A classic cruiser motorcycle with a robust V-twin engine, known for its raw power and iconic design.</div>
+      </div>
+      </div>
+     
+      {/* 3*/}
+      <div className="flex flex-col border p-3 rounded-md">
+      <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Best for Off-Road Adventures</div>
+      <img className="h-60 sm:h-40  lg:h-72 w-full object-cover " src="./c3.jpeg"/>
+      <div className="flex flex-col items-center justify-center space-y-2 bg-white rounded-b-md border-t  p-3">
+      <div className={`${jost.className} text-md`}>Harley-Davidson Sportster 883</div>
+      <div style={{fontWeight:300}} className={`${jost.className} text-[#504d4d]`}>A classic cruiser motorcycle with a robust V-twin engine, known for its raw power and iconic design.</div>
+      </div>
+      </div>
+     
+
+
     </div>
     </div>
     </div>
--- app/HomepageComponents/PopularCompany.tsx
+++ app/HomepageComponents/PopularCompany.tsx
@@ -15,9 +15,11 @@ const PopularCompany = () => {
     <section className="bg-background overflow-hidden py-10">
             <div className="group relative m-auto max-w-7xl px-6 pb-9 border-b ">
                 <div className="flex flex-col items-center space-y-9 ">
-                    <div className="w-full flex items-start justify-center">
+                    <div className="w-full flex flex-col items-start justify-center">
                         {/* <p className="text-end text-sm">Best Companies</p> */}
-                        <div className={`${jost.className} text-black/80 text-3xl md:text-4xl border-b-4 pb-2 `}>Top Companies</div>
+                        <div className={`${jost.className} text-black/80 text-3xl md:text-4xl `}>Our Trusted Partners & Brands</div>
+       <div style={{fontWeight:400}} className={`${jost.className}  text-md text-[#879099]  `}>Lorem ipsum dolor sit amet elit, sed do eiusmod tempor </div> 
+
                     </div>
                     <div className="relative py-6 md:w-[calc(100%-11rem)]">
                         <InfiniteSlider
