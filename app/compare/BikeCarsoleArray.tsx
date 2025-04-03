62766d875c4fec2869030af4ca5036af4870713d
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-03 11:09:06 +0500

    Updated data

--- app/compare/BikeCarsoleArray.tsx
+++ app/compare/BikeCarsoleArray.tsx
@@ -25,37 +25,37 @@ const BikeCarsoleArray = () => {
 
 <Carousel>
   <CarouselContent className="gap-8">
-    <CarouselItem className="md:basis-1/2   border h-36 ">
-      <img className=" w-full object-cover  rounded-t-md " src="./honda-125.jpeg"/>
-        <div className="flex flex-col  p-3 rounded-md">
+    <CarouselItem className="md:basis-1/2   border">
+        <div className="flex flex-col  p-3 rounded-md  ">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Honda CG 125</div>
+      <img className=" w-full object-cover  rounded-t-md " src="./honda-125.jpeg"/>
       </div>
       </CarouselItem>
-    <CarouselItem className="md:basis-1/2 h-1/2 bg-white border">
+    <CarouselItem className="md:basis-1/2  bg-white border">
       <img className=" w-full object-cover rounded-t-md " src="./honda-pridor.jpg"/>
       <div className="flex flex-col  p-3 rounded-md">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Honda Pridor</div>
       </div>
       </CarouselItem>
-    <CarouselItem className="md:basis-1/2 h-1/2  bg-white border">
+    <CarouselItem className="md:basis-1/2   bg-white border">
       <img className=" w-full object-cover rounded-t-md " src="./suzuki-150.jpg"/>
       <div className="flex flex-col  p-3 rounded-md">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Suzuki GS 150</div>
       </div>
       </CarouselItem>
-    <CarouselItem className="md:basis-1/2  h-1/2 bg-white border">
+    <CarouselItem className="md:basis-1/2   bg-white border">
       <img className=" w-full object-cover rounded-t-md " src="./ybr-125.jpg"/>
       <div className="flex flex-col  p-3 rounded-md">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Ybr 125</div>
       </div>
       </CarouselItem>
-    <CarouselItem className="md:basis-1/2 h-1/2 bg-white border">
+    <CarouselItem className="md:basis-1/2  bg-white border">
       <img className=" w-full object-cover rounded-t-md " src="./suzuki-gr.jpg"/>
       <div className="flex flex-col  p-3 rounded-md">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Suzuki GR </div>
       </div>
       </CarouselItem>
-    <CarouselItem className="md:basis-1/2 h-1/2 bg-white border">
+    <CarouselItem className="md:basis-1/2  bg-white border">
       <div className="flex flex-col  p-3 rounded-md">
       <div className={`${jost.className} w-full flex items-center justify-center text-xl`}>Honda CB 150F</div>
       <img className=" w-full object-cover rounded-t-md " src="./handa-cb-150f.jpg"/>
