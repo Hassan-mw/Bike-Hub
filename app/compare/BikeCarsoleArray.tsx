46942d75b4a3b44a997f2230a688c132afc94bf4
Hassan <hmwsoftwaredevelopers@gmail.com>
2025-04-04 23:10:42 +0500

    Updated data

--- app/compare/BikeCarsoleArray.tsx
+++ app/compare/BikeCarsoleArray.tsx
@@ -18,12 +18,12 @@ const jost=Jost({
 const BikeCarsoleArray = () => {
 
 const data=[
-  {image:"/Aprilia RSV4-c.webp",name:"Aprilia RSV4",price:"234",review:"45",url:"aprilia-rsv4"},
-  {image:"/Triump  Tiger 900.jpg",name:"Triump  Tiger 900",price:"333",review:"34",url:"triumph-tiger-900"},
-  {image:"/user-1742580567136.jpeg",name:"Ducatio Multistrada V4  ",price:"234",review:"33",url:"ducati-multistrada-v4"},
+  {image:"/user-1742579562995.jpeg",name:"Aprilia RSV4",price:"234",review:"45",url:"aprilia-rsv4"},
+  {image:"/user-1742578949550.jpeg",name:"yamaha 150",price:"333",review:"34",url:"triumph-tiger-900"},
+  {image:"/user-1742580909704.jpeg",name:"Ducatio Multistrada V4  ",price:"234",review:"33",url:"ducati-multistrada-v4"},
   {image:"/Suzuki SV650.jpeg",name:"Suzuki SV650 ",price:"234",review:"33",url:"ducati-multistrada-v4"},
   {image:"/BMW F850GS.jpeg",name:"BMW F850GS",price:"234",review:"44",url:"aprilia-tuono-rsv4"},
-  {image:"/Ducati Multistrada 1260 Enduro.jpeg",name:" Ducati Multistrada 1260 Enduro",price:"",review:"35",url:"ktm-duke-390"},
+  {image:"/user-1742580688893.jpeg",name:" Ducati Multistrada 1260 Enduro",price:"",review:"35",url:"ktm-duke-390"},
 ]
 
   return (
@@ -38,12 +38,12 @@ const data=[
 
   {
     data.map((data,index)=>
-    <CarouselItem key={data.name}  className="sm:basis-1/2  md:basis-1/3  h-1/2  rounded-lg ">
+    <CarouselItem key={data.name}  className="sm:basis-1/2  md:basis-1/3  h-1/2  rounded-lg bg-white ">
       <Image height={300} width={300} alt={data.name} className=" w-full object-cover rounded-t-md  max-h-[250px]" src={data.image}/>
       <div className="flex flex-col  p-3 rounded-md space-y-1">
       <div className="flex flex-col  p-3 rounded-md space-y-1/2">
       <div style={{fontWeight:300}}  className={`${jost.className} w-full flex items-center justify-center text-xl text-blue-500`}>{data.name}</div>
-      <div style={{fontWeight:300}}  className={`${jost.className} w-full flex items-center justify-center text-sm text-green-500`}>PKR {data.price}</div>
+      <div style={{fontWeight:200}}  className={`${jost.className} w-full flex items-center justify-center text-sm text-green-500`}>PKR {data.price}</div>
       </div>
       <div  style={{fontWeight:300}}  className={`${jost.className} w-full flex items-center justify-center text-xs text-slate-500`}>⭐⭐⭐⭐ {data.review} REviews</div>
       </div>
